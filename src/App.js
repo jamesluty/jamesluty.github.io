c361debade17e2bd222fcc1577e8b8d281e87ad9
James Luty <luty0125@gmail.com>
2022-10-16 12:55:46 -0700

    Update CSS and Banner Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Banner from './components/Banner';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <About/>
       <Projects/>
       <Skills/>
+      <Experience/>
+      <Education/>
+      <Contact/>
     </div>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,8 @@ import banner_bg from '../imgs/banner_bg.jpg';
 const Banner = () => {
     return (
         <div className="banner" style={{ backgroundImage: `url(${banner_bg})` }}>
-            <h1 className="position">Software Engineer</h1>
-            <h1 className="position2">Freelance Web Developer</h1>
+            <h1 className="jobType1">Software Engineer</h1>
+            <h1 className="jobType2">Web Developer</h1>
             <h2>Java | Python | React | HTML | CSS | Javascript</h2>
         </div>
     )
