e8a31f89da2419722de29f16c37a63a8de415676
James Luty <luty0125@gmail.com>
2022-11-02 15:32:06 -0700

    Added bounce text when hovering skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -32,31 +32,31 @@ const Skills = () => {
                         <div className="iconDiv">
                             <img src={java_icon} alt="java" className="icon"/>
                         </div>
-                        <h2 className="skillName">Java</h2>
+                        <h3 className="skillName">Java</h3>
                     </div>
                     <div className="skillsIcon">
                         <div className="iconDiv">
                             <img src={python_icon} alt="python" className="icon"/>
                         </div>
-                        <h2 className="skillName">Python</h2>
+                        <h3 className="skillName">Python</h3>
                     </div>
                     <div className="skillsIcon">
                         <div className="iconDiv">
                             <img src={js_icon} alt="javascript" className="icon"/>
                         </div>
-                        <h2 className="skillName">JavaScript</h2>
+                        <h3 className="skillName">JavaScript</h3>
                     </div>
                     <div className="skillsIcon">
                         <div className="iconDiv">
                             <img src={html_icon} alt="html" className="icon"/>
                         </div>
-                        <h2 className="skillName">HTML</h2>
+                        <h3 className="skillName">HTML</h3>
                     </div>
                     <div className="skillsIcon">
                         <div className="iconDiv">
                             <img src={css_icon} alt="css" className="icon"/>
                         </div>
-                        <h2 className="skillName">CSS</h2>
+                        <h3 className="skillName">CSS</h3>
                     </div>
                 </div>
                 <hr />
@@ -68,37 +68,37 @@ const Skills = () => {
                             <div className="iconDiv">
                                 <img src={spring_icon} alt="spring" className="icon"/>
                             </div>
-                            <h2 className="skillName">Spring Boot</h2>
+                            <h3 className="skillName">Spring Boot</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={flask_icon} alt="flask" className="icon"/>
                             </div>
-                            <h2 className="skillName">Flask</h2>
+                            <h3 className="skillName">Flask</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={node_icon} alt="node" className="icon"/>
                             </div>
-                            <h2 className="skillName">Node.js</h2>
+                            <h3 className="skillName">Node.js</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={react_icon} alt="react" className="icon"/>
                             </div>
-                            <h2 className="skillName">React.js</h2>
+                            <h3 className="skillName">React.js</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={express_icon} alt="express" className="icon"/>
                             </div>
-                            <h2 className="skillName">Express.js</h2>
+                            <h3 className="skillName">Express.js</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={bootstrap_icon} alt="bootstrap" className="icon"/>
                             </div>
-                            <h2 className="skillName">BootStrap</h2>
+                            <h3 className="skillName">BootStrap</h3>
                         </div>
                     </div>
                 </div>
@@ -111,13 +111,13 @@ const Skills = () => {
                             <div className="iconDiv">
                                 <img src={mysql_icon} alt="mysql" className="icon"/>
                             </div>
-                            <h2 className="skillName">MySQL</h2>
+                            <h3 className="skillName">MySQL</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={mongodb_icon} alt="mongodb" className="icon"/>
                             </div>
-                            <h2 className="skillName">MongoDB</h2>
+                            <h3 className="skillName">MongoDB</h3>
                         </div>
                     </div>
                 </div>
@@ -130,37 +130,37 @@ const Skills = () => {
                             <div className="iconDiv">
                                 <img src={json_icon} alt="json" className="icon"/>
                             </div>
-                            <h2 className="skillName">JSON</h2>
+                            <h3 className="skillName">JSON</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={git_icon} alt="git" className="icon"/>
                             </div>
-                            <h2 className="skillName">GIT</h2>
+                            <h3 className="skillName">GIT</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={jenkins_icon} alt="jenkins" className="icon"/>
                             </div>
-                            <h2 className="skillName">Jenkins</h2>
+                            <h3 className="skillName">Jenkins</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={jira_icon} alt="jira" className="icon"/>
                             </div>
-                            <h2 className="skillName">Jira</h2>
+                            <h3 className="skillName">Jira</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={confluence_icon} alt="confluence" className="icon"/>
                             </div>
-                            <h2 className="skillName">Confluence</h2>
+                            <h3 className="skillName">Confluence</h3>
                         </div>
                         <div className="skillsIcon">
                             <div className="iconDiv">
                                 <img src={postman_icon} alt="postman" className="icon"/>
                             </div>
-                            <h2 className="skillName">Postman</h2>
+                            <h3 className="skillName">Postman</h3>
                         </div>
                     </div>
                 </div>
