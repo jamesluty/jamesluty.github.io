d4108013047c754ee3412987680ff18dd2b1b94b
James Luty <luty0125@gmail.com>
2022-11-22 18:29:24 -0800

    Navbar switch to dropdown menu on smaller screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,66 @@
 import React from 'react'
+import menu_icon from '../imgs/menu_icon.png'
 
 const Navbar = () => {
+
+    const menuOpen = () => {
+        document.getElementById("menu").style.display = "flex";
+    }
+
+    const menuClose = () => {
+        document.getElementById("menu").style.display = "none";
+    }
 
     return (
         <div className="navbar">
             <div className="navTitle">
                 <h1>James Luty</h1>
             </div>
-            <ul className="navWrapper sm">
-                <li className='navLink'>
-                    <a href="#about">About</a>
-                </li>
-                <li className='navLink'>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li className='navLink'>
-                    <a href="#skills">Skills</a>
-                </li>
-                <li className='navLink'>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li className='navLink'>
-                    <a href="#education">Education</a>
-                </li>
-                <li className='navLink'>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
+            <div className="lgNavbar">
+                <ul className="navWrapper sm">
+                    <li className='navLink'>
+                        <a href="#about">About</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#education">Education</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div onMouseOver={menuOpen} onMouseOut={menuClose} className="mdNavbar">
+                <img src={menu_icon} alt="menu" className="menuIcon"/>
+                <ul className="navWrapper sm" id="menu">
+                    <li className='navLink'>
+                        <a href="#about">About</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#education">Education</a>
+                    </li>
+                    <li className='navLink'>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
