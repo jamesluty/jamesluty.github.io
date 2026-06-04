f86f8ad41d6341be3e9e8c7f29cc14877ac7787a
James Luty <luty0125@gmail.com>
2026-06-04 12:02:30 -0700

    Update banner text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import banner_bg from '../imgs/banner_bg.jpg';
 const Banner = () => {
     return (
         <div className="banner" style={{ backgroundImage: `url(${banner_bg})` }}>
-            <h1 className="jobType1">Quality Assurance Engineer</h1>
+            <h1 className="jobType1">Quality Assurance</h1>
             <h1 className="jobType1">Software Test Engineer</h1>
             <h2 className="jobSkills">Java | Python | React | HTML | CSS | Javascript | Selenium</h2>
         </div>
